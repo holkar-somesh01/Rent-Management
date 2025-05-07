8fe89f02af2b3a225d03a2e10c0b50d7d920e57a
someshwarsholkar <someshwarsholkar22@gmail.com>
2025-05-07 22:28:50 +0530

    frontend build and add

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(cors({
 
 app.use(cookieParser())
 app.use(express.json())
-// app.use(express.static("dist"))
+app.use(express.static("dist"))
 
 app.use('/api/v1/auth', require('./routes/auth.routes'))
 app.use('/api/v1/admin', require('./routes/admin.routes'))
