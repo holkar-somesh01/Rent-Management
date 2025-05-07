9568cfe847d34ca91b44728cb37bffeb80810f8c
someshwarsholkar <someshwarsholkar22@gmail.com>
2025-05-07 22:34:52 +0530

    resource hide

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use('/api/v1/user', require('./routes/user.routes'))
 
 app.use("*", (req, res) => {
     res.sendFile(path.join(__dirname, "dist", "index.html"))
-    res.status(404).json({ message: "Resource Not Found" })
+    // res.status(404).json({ message: "Resource Not Found" })
 })
 app.use((err, req, res, next) => {
     console.log(err)
